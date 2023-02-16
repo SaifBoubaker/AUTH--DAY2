de04812790dc5c87d9fda7593260fb1781dfa152
SaifBoubaker <saif.boubaker.contact@gmail.com>
2023-02-16 21:06:20 +0100

    register

---
+++ client/src/Redux/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+import UserReducer from "./Slices/UserSlice";
+const store = configureStore({
+  reducer: { userAuth: UserReducer },
+});
+export default store;
